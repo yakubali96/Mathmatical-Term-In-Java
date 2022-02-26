b42a7ac1c69b29cbe26211951170a5e2e86ee11e
yakub ali <ali1263399@gmail.com>
2022-02-27 04:30:53 +0600

    some math commit

--- src/SirProject/PrimeNumber.java
+++ src/SirProject/PrimeNumber.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SirProject;
 
 import java.util.Scanner;
 
-/**
- *
- * @author User
- */
 public class PrimeNumber {
 
     public static void main(String[] args) {
